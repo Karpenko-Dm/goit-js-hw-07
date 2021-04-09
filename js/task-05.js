a71783b7cfcf7f28c1989b23ff893ab6ed7860b8
Karpenko-Dm <73794678+Karpenko-Dm@users.noreply.github.com>
2021-04-09 22:57:38 +0300

    create task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,19 @@
 const input = document.querySelector('[id= "name-input"]');
 const nameLabel = document.querySelector('[id="name-output"]');
-input.addEventListener('input', onInputChange);
-
+let strangerUser = 'незнакомец'
 
 function onInputChange(event) {
-    console.log(event.currentTarget.value);
-    nameLabel.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === '') {
+         nameLabel.textContent = strangerUser;
+     
+    } else {
+          nameLabel.textContent = event.currentTarget.value;
+    }
 }
+input.addEventListener('input', onInputChange);
+  
+    
+    
 
 
 
